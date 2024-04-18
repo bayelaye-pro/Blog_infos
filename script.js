40be53a1dcdd0cfebb701b88572b0b9fdb9a5c64
ibniang <108143877+bayelaye-pro@users.noreply.github.com>
2024-04-18 13:15:36 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Senior Manager", "Business Administrator", "Global Leadership Program Washington DC fellow"],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
